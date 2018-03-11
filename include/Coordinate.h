1ad8203455d5ec4372e26022e664778bb26f290b
Ghostkeeper <rubend@tutanota.com>
2018-03-11 16:38:41 +0100

    Add coord_t typedef
    This will abstract the type out so that users don't need to know the underlying implementation.

---
+++ include/Coordinate.h
@@ -0,0 +1,21 @@
+/*
+ * Library for performing massively parallel computations on polygons.
+ * Copyright (C) 2018 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+#ifndef COORDINATE_H
+#define COORDINATE_H
+
+#include <stdint.h>
+
+namespace parallelogram {
+
+typedef int64_t coord_t; //Needs to be a fixed-length type in order to communicate with OpenCL.
+
+}
+
+#endif //COORDINATE_H
+
