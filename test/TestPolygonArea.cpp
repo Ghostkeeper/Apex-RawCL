6b74f09f491ff41decbd24e0dd894f366ce31bd4
Ghostkeeper <rubend@tutanota.com>
2018-03-11 19:01:16 +0100

    Add tests
    This usses Google Test to run it and then CTest to run the Google Tests via CMake. It's a bit of a trick to get the tests to actually compile, but it works well like this.

---
+++ test/TestPolygonArea.cpp
@@ -0,0 +1,30 @@
+/*
+ * Library for performing massively parallel computations on polygons.
+ * Copyright (C) 2018 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+#ifndef TESTPOLYGONAREA
+#define TESTPOLYGONAREA
+
+#include <gtest/gtest.h>
+#include "Polygon.h"
+
+namespace parallelogram {
+
+TEST(TestPolygonArea, InitialAreaIsZero) {
+	Polygon empty_polygon;
+	EXPECT_EQ(0, empty_polygon.area());
+}
+
+}
+
+int main(int argc, char* argv[]) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+#endif //TESTPOLYGONAREA
+
