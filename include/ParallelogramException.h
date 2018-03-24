c377af1ffc15d5775ed716240c727dc3c99e944c
Ghostkeeper <rubend@tutanota.com>
2018-03-24 13:43:55 +0100

    Move includes into header guard
    We only need to include them once, right.

--- include/ParallelogramException.h
+++ include/ParallelogramException.h
@@ -6,10 +6,10 @@
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
  */
 
-#include <exception> //To extend from the built-in exception class.
-
 #ifndef PARALLELOGRAMEXCEPTION_H
 #define PARALLELOGRAMEXCEPTION_H
+
+#include <exception> //To extend from the built-in exception class.
 
 namespace parallelogram {
 
