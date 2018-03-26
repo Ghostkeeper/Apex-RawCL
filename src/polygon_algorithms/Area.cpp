1c876a4b85f929cb5f2100ee153a0815219a6e87
Ghostkeeper <rubend@tutanota.com>
2018-03-26 23:44:01 +0200

    Build OpenCL pipeline and passes on global memory
    This sets up the basic structure to call OpenCL with: A device, a context, a queue and some initial buffer filling with input data. What remains to be done is to actually call the kernel, the code in the kernel itself and an output and maybe intermediary buffer.

--- src/polygon_algorithms/Area.cpp
+++ src/polygon_algorithms/Area.cpp
@@ -6,13 +6,79 @@
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
  */
 
-#include "SimplePolygon.h"
+#include <algorithm> //For std::min.
+#ifdef __APPLE__
+	#include "OpenCL/opencl.h"
+#else
+	#include "CL/cl.h"
+#endif
+
+#include "OpenCLDevices.h" //To get the OpenCL devices we can run on.
+#include "ParallelogramException.h"
+#include "SimplePolygon.h" //We're implementing functions from this header.
 
 namespace parallelogram {
 
 coord_t SimplePolygon::area_gpu() const {
-	//TODO: Implement this.
-	return area_host();
+	//TODO: We want to get the preferred device from the benchmarks::choose function.
+	OpenCLDevices& devices = OpenCLDevices::getInstance();
+	cl::Device device;
+	if(!devices.getGPUs().empty()) {
+		device = devices.getGPUs()[0];
+	} else if(!devices.getCPUs().empty()) {
+		device = devices.getCPUs()[0];
+	} else {
+		throw ParallelogramException("No supported OpenCL devices!");
+	}
+
+	cl::Context context({device});
+	cl::CommandQueue queue(context, device);
+
+	//Load the source code.
+	cl::Program::Sources kernel_sources;
+	const std::string kernel_source = R"kernel(
+//TODO.
+)kernel";
+	kernel_sources.push_back({kernel_source.c_str(), kernel_source.length()});
+	cl::Program program(context, kernel_sources);
+	if(program.build({device}) != CL_SUCCESS) {
+		throw ParallelogramException("Compiling kernel for Polygon::area failed.");
+	}
+
+	//Allocate constant memory on the device for the input.
+	cl_ulong constant_buffer_size;
+	if(device.getInfo(CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE, &constant_buffer_size) != CL_SUCCESS) {
+		constant_buffer_size = 64 << 10; //OpenCL standard says that the minimum is 64kB.
+	}
+	const size_t vertices_per_pass = constant_buffer_size / (sizeof(coord_t) * 2);
+	coord_t total_area = 0; //Result sum of all passes.
+	for(size_t pivot_vertex = 0; pivot_vertex < size(); pivot_vertex += vertices_per_pass - 2) { //If the total data size is more than what fits in constant memory, we'll have to make multiple passes.
+		/* Each pass will compute the area of a part of the polygon.
+		 * We'll take a part of the perimeter and close that polyline off with
+		 * the initial vertex to create a polygon and compute that area. We save
+		 * the beginning and ending vertex of each such polygon to compute the
+		 * area that we missed in the middle of these pieces of polygon (the
+		 * pivot vertices). */
+
+		size_t pivot_vertex_after = pivot_vertex + vertices_per_pass - 2; //-1 because we need to store the pivot_vertex twice, and -1 because the pivot vertex of the next pass is the last vertex of this pass.
+		size_t vertices_this_pass = vertices_per_pass;
+		if(pivot_vertex_after >= size()) {
+			pivot_vertex_after = 0;
+			vertices_this_pass = size() - pivot_vertex + 1;
+		}
+
+		cl_ulong this_constant_buffer_size = vertices_this_pass * sizeof(coord_t) * 2;
+		cl::Buffer input_points(context, CL_MEM_READ_ONLY, this_constant_buffer_size);
+		queue.enqueueWriteBuffer(input_points, CL_TRUE, 0, this_constant_buffer_size - sizeof(coord_t) * 4, &(*this)[pivot_vertex]); //Write the polyline and first pivot vertex.
+		queue.enqueueWriteBuffer(input_points, CL_TRUE, this_constant_buffer_size - sizeof(coord_t) * 4, sizeof(coord_t) * 2, &(*this)[pivot_vertex_after]); //Write the second pivot vertex.
+		queue.enqueueWriteBuffer(input_points, CL_TRUE, this_constant_buffer_size - sizeof(coord_t) * 2, sizeof(coord_t) * 2, &(*this)[pivot_vertex]); //Write the first pivot vertex again to close the polygon.
+
+		//TODO: Call the kernel to compute the area of this polygon and add it to total_area.
+
+		total_area += (*this)[pivot_vertex].x * (*this)[pivot_vertex_after].y - (*this)[pivot_vertex].y * (*this)[pivot_vertex_after].x;
+	}
+
+	return total_area;
 }
 
 coord_t SimplePolygon::area_host() const {
