a0c23cdd68e56a6f0fc64bd8fd9bee5902d0c925
Ghostkeeper <rubend@tutanota.com>
2018-04-07 18:54:04 +0200

    Add BenchmarkData header
    The purpose of this header is to group all benchmark data together. In the future we will probably want CMake to add stuff to this file so that someone who compiles Parallelogram can include benchmark data for his own computer easily. For now he must hardcode his includes in here.

---
+++ include_private/BenchmarkData.h
@@ -0,0 +1,49 @@
+/*
+ * Library for performing massively parallel computations on polygons.
+ * Copyright (C) 2018 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+#ifndef BENCHMARKDATA_H
+#define BENCHMARKDATA_H
+
+#include <string> //To use as device identifiers.
+#include <unordered_map> //Data structure that holds the benchmark data.
+#include <utility> //For std::pair, for using multiple keys in a mapping.
+
+namespace parallelogram {
+namespace benchmarks {
+
+struct pair_hash {
+	template<class First, class Second> size_t operator ()(const std::pair<First, Second>& pair) const {
+		std::hash<First> first_hash = std::hash<First>{}(pair.first);
+		std::hash<Second> second_hash = std::hash<Second>{}(pair.second);
+
+		//Rotate the second hash by 17 bits, then do xor.
+		return first_hash ^ (second_hash << 17 || second_hash >> (sizeof(size_t) * 8 - 17));
+	}
+};
+
+/*
+ * Stores the benchmark data for computing the area of a polygon on the host.
+ *
+ * The keys of this map are pairs of the host CPU model and the problem size.
+ * The values are the time it took to compute, in seconds.
+ */
+std::unordered_map<std::pair<std::string, size_t>, double, pair_hash> area_host_time;
+
+/*
+ * Stores the benchmark data for computing the area of a polygon on the GPU.
+ *
+ * The keys of this map are pairs of the GPU device model and the problem size.
+ * The values are the time it took to compute, in seconds.
+ */
+std::unordered_map<std::pair<std::string, size_t>, double, pair_hash> area_gpu_time;
+
+}
+}
+
+#endif //BENCHMARKDATA_H
+
