96247a08181518b988069a1b76b31834ff0791c6
Ghostkeeper <rubend@tutanota.com>
2018-05-06 18:34:58 +0200

    Download Eigen from official repository instead of clone
    And download them by requested version number, so we don't need to detect the version number from source code any more. That should make this a bit faster.

--- cmake/modules/FindEigen.cmake
+++ cmake/modules/FindEigen.cmake
@@ -52,37 +52,21 @@ else()
 	option(BUILD_EIGEN "Build Eigen from source." ON) #This is a lie actually, since Eigen is header-only and doesn't need any compiling.
 	if(BUILD_EIGEN)
 		message(STATUS "Building Eigen from source.")
-		#include(ExternalProject)
-		#ExternalProject_Add(Eigen
-		#	GIT_REPOSITORY https://github.com/eigenteam/eigen-git-mirror #We're using the mirror so that we don't need to depend on Mercurial as well as Git. Just Git now.
-		#	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-		#	INSTALL_COMMAND "" #Don't immediately install it. We just need the headers.
-		#)
-		#set(EIGEN_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/eigen")
-		#set(EIGEN_FOUND TRUE)
+		set(EIGEN_MAJOR_VERSION 3) #The version that we want.
+		set(EIGEN_MINOR_VERSION 3)
+		set(EIGEN_PATCH_VERSION 4)
 
-		#Getting Eigen's version number from their source code!
 		#First download and extract the source code.
-		file(DOWNLOAD https://github.com/eigenteam/eigen-git-mirror/archive/master.zip "${CMAKE_CURRENT_BINARY_DIR}/eigen-git-mirror-master.zip")
+		file(DOWNLOAD http://bitbucket.org/eigen/eigen/get/${EIGEN_MAJOR_VERSION}.${EIGEN_MINOR_VERSION}.${EIGEN_PATCH_VERSION}.zip "${CMAKE_CURRENT_BINARY_DIR}/eigen.zip")
 		execute_process(
-			COMMAND ${CMAKE_COMMAND} -E tar x "${CMAKE_CURRENT_BINARY_DIR}/eigen-git-mirror-master.zip"
+			COMMAND ${CMAKE_COMMAND} -E tar x "${CMAKE_CURRENT_BINARY_DIR}/eigen.zip"
 			WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
 		)
-		set(EIGEN_INCLUDE_DIRS "${CMAKE_CURRENT_BINARY_DIR}/eigen-git-mirror-master")
+		#Eigen packages these zips with their commit ID, which is unknown. Use glob to figure out what the folder is there.
+		file(GLOB _eigen_recogniser "${CMAKE_CURRENT_BINARY_DIR}/eigen-eigen-*/eigen3.pc.in")
+		message(STATUS "Eigen recogniser: ${_eigen_recogniser}")
+		get_filename_component(EIGEN_INCLUDE_DIRS, ${_eigen_recogniser} DIRECTORY)
 		set(EIGEN_FOUND TRUE)
-		#Take the macro file that contains the version number and append a program to it that outputs the values of the macro.
-		file(READ "${EIGEN_INCLUDE_DIRS}/Eigen/src/Core/util/Macros.h" _eigen_macros_file)
-		file(READ "${CMAKE_MODULE_PATH}/GetEigenVersion/GetEigenVersion.cpp" _get_eigen_version_original)
-		file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/GetEigenVersionPatched.cpp" "#define EIGEN_DEVICE_FUNC\n\n${_eigen_macros_file}\n\n${_get_eigen_version_original}") #Append a simple program to one of the macro files.
-		#Compile this program, get its result.
-		try_run(RUN_RESULT COMPILE_RESULT
-			"${CMAKE_CURRENT_BINARY_DIR}"
-			"${CMAKE_CURRENT_BINARY_DIR}/GetEigenVersionPatched.cpp"
-			RUN_OUTPUT_VARIABLE _run_output #The output of this application is #;#;#, so due to the semicolons it gets interpreted as a list in CMake.
-		)
-		list(GET _run_output 0 EIGEN_MAJOR_VERSION)
-		list(GET _run_output 1 EIGEN_MINOR_VERSION)
-		list(GET _run_output 2 EIGEN_PATCH_VERSION)
 		set(EIGEN_VERSION ${EIGEN_MAJOR_VERSION}.${EIGEN_MINOR_VERSION}.${EIGEN_PATCH_VERSION})
 		message(STATUS "Found Eigen version: ${EIGEN_VERSION}")
 	endif()
