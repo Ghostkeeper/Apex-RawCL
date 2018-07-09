49525b1fee666848b4a3a8b7c07fb1eade202ace
Ghostkeeper <rubend@tutanota.com>
2018-07-10 00:07:32 +0200

    Implement simple form of containment checking
    This algorithm is not done yet, since it doesn't handle edge cases at all yet.

--- src/polygon_algorithms/Contains.cpp
+++ src/polygon_algorithms/Contains.cpp
@@ -6,12 +6,48 @@
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
  */
 
+#include <iostream> //DEBUG!
 #include "SimplePolygon.h" //The class whose algorithms we're implementing.
 
 namespace parallelogram {
 
 bool SimplePolygon::contains_host(const Point2& point, const FillType& fill_type) const {
-	return false; //TODO.
+	if(size() < 3) {
+		return false; //TODO: Check edge cases for lines and points if edges are to be included. Currently they're not included.
+	}
+	//This is the winding number algorithm to determine if a point is inside a polygon.
+	int winding_number = 0;
+	Point2 previous = back();
+	for(size_t index = 0; index < size(); index++) //Take two adjacent vertices of the polygon.
+	{
+		const Point2& next = (*this)[index];
+		/* Cast a ray from the point towards the right and figure out whether
+		the line segment between these two vertices crosses it and in which
+		direction. */
+		if(previous.y < next.y) { //Rising edge.
+			if(point.y > previous.y && point.y < next.y) { //Crosses height of point.
+				if(point.isLeftOfLineSegment(previous, next) > 0) { //Line is right of point.
+					winding_number++;
+				}
+			}
+		} else if(previous.y > next.y) { //Falling edge (next vertex is lower than previous vertex).
+			if(point.y < previous.y && point.y > next.y) { //Crosses height of point.
+				if(point.isLeftOfLineSegment(previous, next) < 0) { //Line is left of point.
+					winding_number--;
+				}
+			}
+		}
+
+		previous = next;
+	}
+
+	switch(fill_type) {
+		default:
+		case FillType::EVEN_ODD:
+			return winding_number % 2 == 1;
+		case FillType::NONZERO:
+			return winding_number != 0;
+	}
 }
 
 }
