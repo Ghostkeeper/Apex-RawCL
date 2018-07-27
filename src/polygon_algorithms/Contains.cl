83676d652c67c9d6b042d7052947f80040c8ddcf
Ghostkeeper <rubend@tutanota.com>
2018-07-27 23:54:56 +0200

    Implement contains() on OpenCL
    It gives the wrong output however. Always seems to be 0 windings?

---
+++ src/polygon_algorithms/Contains.cl
@@ -0,0 +1,78 @@
+/*
+ * Library for performing massively parallel computations on polygons.
+ * Copyright (C) 2018 Ghostkeeper
+ * This library is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
+ * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
+ */
+
+R"kernel(
+
+/*
+ * Computes the winding number of (a part of) a polygon around a certain point.
+ *
+ * Adding all the winding numbers of every edge together gives us the actual
+ * winding number of the polygon around the point, which allows us to determine
+ * if the point should be considered inside the polygon.
+ * \param input_data_points The coordinates of the vertices of the polygon.
+ * \param total_vertices The total number of vertices in the entire polygon (not
+ * just this work group).
+ * \param point The point to wind around.
+ * \param include_edges Whether to include edges inside the polygon or not.
+ * \param output_windings For each work group, the winding number for their part
+ * of the polygon.
+ * \param sums Some scratch space to store the computed winding numbers before
+ * summing them all up within one work group.
+ */
+void kernel contains(global const int2* input_data_points, const long total_vertices, const int2 point, const int include_edges, global long* output_windings, local long* sums) {
+	//Compute the winding number contributed by one line segment.
+	const uint global_id = get_global_id(0);
+	const int2 previous = input_data_points[global_id];
+	const int2 next = input_data_points[global_id + 1];
+	const uint local_id = get_local_id(0);
+
+	//The winding number algorithm outlined in Contains.cpp, but with less branching.
+	if(previous.y == next.y && previous.y == point.y) { //Horizontal edges are very special. Have to use branching for that, sorry.
+		sums[local_id] = 0; //TODO.
+	} else {
+		const int is_rising = (next.y - previous.y) % 1; //1 if the edge is going up, -1 if it's going down.
+		const uint point_delta_y = point.y - next.y; //Trick to prevent a branch here: Let unsigned int underflow if y is less than next.y.
+		const uint prev_delta_y = previous.y - next.y;
+		if(point_delta_y * is_rising > prev_delta_y * is_rising) {
+			const int is_left = (next.x - previous.x) * (point.y - previous.y) - (next.y - previous.y) * (point.x - previous.x) * is_rising;
+			sums[local_id] = is_left > 0 || (is_left == 0 && include_edges ^ (is_rising < 0));
+		}
+	}
+
+	//Aggregate sum on the memory in this work group.
+	const uint local_size = get_local_size(0);
+	const uint workgroup_id = global_id / local_size;
+	uint current_size = local_size;
+	if((workgroup_id + 1) * local_size > total_vertices) {
+		if(workgroup_id * local_size < total_vertices) {
+			current_size = total_vertices % local_size;
+		} else {
+			current_size = 0;
+		}
+	}
+	for(int offset = (current_size + 1) / 2; offset > 1; offset = (current_size + 1) / 2) {
+		barrier(CLK_LOCAL_MEM_FENCE);
+		if(local_id < offset && local_id + offset < current_size) {
+			sums[local_id] += sums[local_id + offset];
+		}
+		current_size = offset;
+	}
+
+	//Copy the resulting sum to the output.
+	barrier(CLK_LOCAL_MEM_FENCE);
+	if(local_id == 0) {
+		if(current_size > 1) { //Last iteration of the aggregate sum.
+			sums[local_id] += sums[local_id + 1];
+		} else if(current_size == 0) { //Edge case for work groups whose work was indivisible such that they got 0 work.
+			sums[local_id] = 0;
+		}
+		output_windings[workgroup_id] = sums[local_id];
+	}
+}
+
+)kernel"
--- src/polygon_algorithms/Contains.cpp
+++ src/polygon_algorithms/Contains.cpp
@@ -6,12 +6,94 @@
  * You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
  */
 
+#include "OpenCL.h" //To compute point-in-polygon via OpenCL.
+
+#include "DeviceStatistics.h" //To get the capacities of the OpenCL devices to adjust the algorithm to them.
+#include "OpenCLContext.h" //To get the OpenCL context to run on.
+#include "ParallelogramException.h"
 #include "SimplePolygon.h" //The class whose algorithms we're implementing.
 
 namespace parallelogram {
 
 bool SimplePolygon::contains_opencl(const cl::Device& device, const Point2& point, const EdgeInclusion& include_edge, const FillType& fill_type) const {
-	return false;
+	cl::Context& context = OpenCLContext::getInstance().contexts[device];
+	cl::CommandQueue queue(context, device);
+
+	//Load the source code.
+	cl::Program& program = OpenCLContext::getInstance().compile(device,
+		#include "Contains.cl"
+	);
+
+	//We might need to make multiple passes if the device has a very limited amount of memory.
+	DeviceStatistics statistics(&device);
+	constexpr size_t vertex_size = sizeof(coord_t) * 2;
+	const cl_ulong local_buffer_size = statistics.local_memory / vertex_size * vertex_size; //Make sure that the memory buffers hold an integer number of vertices.
+	const cl_ulong global_buffer_size = statistics.global_memory / vertex_size * vertex_size;
+
+	const size_t vertices_per_pass = global_buffer_size / vertex_size;
+	int total_winding = 0; //Result sum of all passes.
+	for(size_t pivot_vertex = 0; pivot_vertex < size(); pivot_vertex += vertices_per_pass - 1) { //If the total data size is more than what fits in constant memory, we'll have to make multiple passes.
+		//Each item works on a line segment, which requires two vertices.
+		//So we must leave space for 1 extra vertex in memory.
+		size_t pivot_vertex_after = pivot_vertex + vertices_per_pass - 1; //-1 because the pivot vertex of the next pass is the last vertex of this pass.
+		size_t vertices_this_pass = vertices_per_pass;
+		if(pivot_vertex_after >= size()) {
+			pivot_vertex_after = 0;
+			vertices_this_pass = size() - pivot_vertex;
+		}
+
+		//Dividing the work over as many work groups as possible.
+		size_t this_work_groups = std::min(static_cast<size_t>(statistics.compute_units), vertices_this_pass);
+		size_t vertices_per_work_group = (vertices_this_pass + this_work_groups - 1) / this_work_groups;
+		vertices_per_work_group = std::min(vertices_per_work_group, statistics.items_per_compute_unit); //However the work group size is limited by hardware and the number of compute units scales then.
+		vertices_per_work_group = std::min(vertices_per_work_group, local_buffer_size / vertex_size); //We must also limit the work group size by the memory that the work groups can use locally.
+		this_work_groups = (vertices_this_pass + vertices_per_work_group - 1) / vertices_per_work_group;
+		//Round the global work size up to multiple of vertices_per_work_group. The kernel itself handles work items that need to idle.
+		const size_t global_work_size = (vertices_this_pass + vertices_per_work_group - 1) / vertices_per_work_group * vertices_per_work_group;
+
+		//Allocate constant memory on the device for the input.
+		const cl_ulong this_constant_buffer_size = (vertices_this_pass + 1) * vertex_size;
+		cl::Buffer input_points(context, CL_MEM_READ_ONLY, (global_work_size + 1) * vertex_size);
+		queue.enqueueWriteBuffer(input_points, CL_TRUE, 0, this_constant_buffer_size - vertex_size, &(*this)[pivot_vertex]); //Write the polyline and first pivot vertex.
+		queue.enqueueWriteBuffer(input_points, CL_TRUE, this_constant_buffer_size - vertex_size, vertex_size, &(*this)[pivot_vertex_after]); //Write the second pivot vertex.
+
+		//Allocate an output buffer: One int for each work group as their output.
+		cl_ulong this_output_buffer_size = this_work_groups * sizeof(cl_int);
+		cl::Buffer output_winding_numbers(context, CL_MEM_WRITE_ONLY, this_output_buffer_size);
+
+		//Call the kernel to compute the winding number of this polygon and add it to total_winding.
+		cl::Kernel contains_kernel(program, "contains");
+		contains_kernel.setArg(0, input_points);
+		contains_kernel.setArg(1, vertices_this_pass);
+		cl_int2 point_vector = {point.x, point.y};
+		contains_kernel.setArg(2, point_vector);
+		contains_kernel.setArg(3, include_edge == EdgeInclusion::INSIDE);
+		contains_kernel.setArg(4, output_winding_numbers);
+		contains_kernel.setArg(5, cl::Local(vertices_per_work_group * sizeof(cl_int)));
+
+		queue.enqueueNDRangeKernel(contains_kernel, cl::NullRange, cl::NDRange(global_work_size), cl::NDRange(vertices_per_work_group));
+		cl_int result = queue.finish(); //Let the device do its thing!
+		if(result != CL_SUCCESS) {
+			throw ParallelogramException("Error executing command queue for point-in-polygon computation.");
+		}
+
+		//Read the output data in.
+		std::vector<int> winding_numbers;
+		winding_numbers.resize(this_work_groups);
+		queue.enqueueReadBuffer(output_winding_numbers, CL_TRUE, 0, this_output_buffer_size, &(winding_numbers[0]));
+		queue.finish();
+		for(const int winding_number : winding_numbers) {
+			total_winding += winding_number;
+		}
+	}
+
+	switch(fill_type) {
+		default:
+		case FillType::EVEN_ODD:
+			return total_winding & 1;
+		case FillType::NONZERO:
+			return total_winding != 0;
+	}
 }
 
 bool SimplePolygon::contains_host(const Point2& point, const EdgeInclusion& include_edge, const FillType& fill_type) const {
