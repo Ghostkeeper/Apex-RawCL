243ad63210e0648b5e2ecf3ee528c422d531c2ca
Ghostkeeper <rubend@tutanota.com>
2018-10-07 00:59:45 +0200

    Fail on first wrong coordinate
    If one of them is wrong, they probably all are. We just need to know about one, I guess.

--- test/TestPolygonTranslate.cpp
+++ test/TestPolygonTranslate.cpp
@@ -27,14 +27,14 @@ TEST(TestPolygonTranslate, SquareMovePositive) {
 
 	square_1000.translate(10, 25);
 
-	EXPECT_EQ(10, square_1000[0][0].x);
-	EXPECT_EQ(25, square_1000[0][0].y);
-	EXPECT_EQ(1010, square_1000[0][1].x);
-	EXPECT_EQ(25, square_1000[0][1].y);
-	EXPECT_EQ(1010, square_1000[0][2].x);
-	EXPECT_EQ(1025, square_1000[0][2].y);
-	EXPECT_EQ(10, square_1000[0][3].x);
-	EXPECT_EQ(1025, square_1000[0][3].y);
+	ASSERT_EQ(10, square_1000[0][0].x);
+	ASSERT_EQ(25, square_1000[0][0].y);
+	ASSERT_EQ(1010, square_1000[0][1].x);
+	ASSERT_EQ(25, square_1000[0][1].y);
+	ASSERT_EQ(1010, square_1000[0][2].x);
+	ASSERT_EQ(1025, square_1000[0][2].y);
+	ASSERT_EQ(10, square_1000[0][3].x);
+	ASSERT_EQ(1025, square_1000[0][3].y);
 }
 
 /*
@@ -55,22 +55,22 @@ TEST(TestPolygonTranslate, SquareDoughnut) {
 
 	doughnut.translate(-5, 5);
 
-	EXPECT_EQ(-5, doughnut[0][0].x);
-	EXPECT_EQ(5, doughnut[0][0].y);
-	EXPECT_EQ(95, doughnut[0][1].x);
-	EXPECT_EQ(5, doughnut[0][1].y);
-	EXPECT_EQ(95, doughnut[0][2].x);
-	EXPECT_EQ(105, doughnut[0][2].y);
-	EXPECT_EQ(-5, doughnut[0][3].x);
-	EXPECT_EQ(105, doughnut[0][3].y);
-	EXPECT_EQ(20, doughnut[1][0].x);
-	EXPECT_EQ(30, doughnut[1][0].y);
-	EXPECT_EQ(20, doughnut[1][1].x);
-	EXPECT_EQ(80, doughnut[1][1].y);
-	EXPECT_EQ(70, doughnut[1][2].x);
-	EXPECT_EQ(80, doughnut[1][2].y);
-	EXPECT_EQ(70, doughnut[1][3].x);
-	EXPECT_EQ(30, doughnut[1][3].y);
+	ASSERT_EQ(-5, doughnut[0][0].x);
+	ASSERT_EQ(5, doughnut[0][0].y);
+	ASSERT_EQ(95, doughnut[0][1].x);
+	ASSERT_EQ(5, doughnut[0][1].y);
+	ASSERT_EQ(95, doughnut[0][2].x);
+	ASSERT_EQ(105, doughnut[0][2].y);
+	ASSERT_EQ(-5, doughnut[0][3].x);
+	ASSERT_EQ(105, doughnut[0][3].y);
+	ASSERT_EQ(20, doughnut[1][0].x);
+	ASSERT_EQ(30, doughnut[1][0].y);
+	ASSERT_EQ(20, doughnut[1][1].x);
+	ASSERT_EQ(80, doughnut[1][1].y);
+	ASSERT_EQ(70, doughnut[1][2].x);
+	ASSERT_EQ(80, doughnut[1][2].y);
+	ASSERT_EQ(70, doughnut[1][3].x);
+	ASSERT_EQ(30, doughnut[1][3].y);
 }
 
 }
