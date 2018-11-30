39a5a54b8ce4f2aab221f0c86c54fd8f0d9a0d50
Ghostkeeper <rubend@tutanota.com>
2018-12-01 00:07:48 +0100

    Split LimitedByWorkGroupSize test up into individual cases
    This way it's easier to see which case is going wrong if we find a bug later.

--- test/TestSimplePolygonBatch.cpp
+++ test/TestSimplePolygonBatch.cpp
@@ -48,15 +48,15 @@ protected:
  *
  * This is the simplest case.
  */
-TEST_F(TestSimplePolygonBatch, LimitedByWorkGroupSize) {
+TEST_F(TestSimplePolygonBatch, Unlimited) {
 	SimplePolygonBatch<std::vector<SimplePolygon>::const_iterator> batch(ten_triangles.cbegin(), ten_triangles.cend());
 	const_groper.tested_batch = &batch;
 
-	//Construct a computer with enough memory for anything, and just enough workers in a group so that everything goes into one group.
+	//Construct a computer with enough memory for anything and unlimited work items per group.
 	DeviceStatistics limits(nullptr);
 	limits.global_memory = 9999999999999; //Shouldn't be a limit.
-	limits.local_memory = 9999999999999; //Shouldn't be a limit.
-	limits.items_per_compute_unit = 9999999999999; //Not a limit yet.
+	limits.local_memory = 9999999999999;
+	limits.items_per_compute_unit = 9999999999999;
 
 	std::vector<std::vector<size_t>> start_positions;
 	std::vector<size_t> work_groups_per_pass;
@@ -67,17 +67,27 @@ TEST_F(TestSimplePolygonBatch, LimitedByWorkGroupSize) {
 	for(size_t i = 0; i < ten_triangles.size(); i++) {
 		EXPECT_EQ(i * 4, start_positions[0][i]); //Each triangle stores 4 vertices (of which one pivot).
 	}
+}
 
-	limits.items_per_compute_unit = 30; //Still not a limit yet.
+TEST_F(TestSimplePolygonBatch, JustEnoughWorkItems) {
+	SimplePolygonBatch<std::vector<SimplePolygon>::const_iterator> batch(ten_triangles.cbegin(), ten_triangles.cend());
+	const_groper.tested_batch = &batch;
+
+	//Construct a computer with enough memory for everything but just barely enough work items per group.
+	DeviceStatistics limits(nullptr);
+	limits.global_memory = 9999999999999; //Shouldn't be a limit.
+	limits.local_memory = 9999999999999; //Shouldn't be a limit.
+	limits.items_per_compute_unit = 30; //Just enough to process all 30 vertices at once.
+
+	std::vector<std::vector<size_t>> start_positions;
+	std::vector<size_t> work_groups_per_pass;
+
 	const_groper.divide_edges(limits, 0, start_positions, work_groups_per_pass);
 	ASSERT_EQ(1, start_positions.size()); //There should still be only one work group.
-	EXPECT_EQ(ten_triangles.size(), start_positions[0].size());
-
-	limits.items_per_compute_unit = 29; //Now we can't fit everything in one work group. Need a second one!
-	const_groper.divide_edges(limits, 0, start_positions, work_groups_per_pass);
-	ASSERT_EQ(2, start_positions.size()); //Now we need 2 work groups.
-	EXPECT_EQ(10, start_positions[0].size()); //All 10 triangles will still start in the first work group.
-	EXPECT_EQ(0, start_positions[1].size()); //None of them start in the second group, but it is there.
+	EXPECT_EQ(ten_triangles.size(), start_positions[0].size()); //And it should contain all 10 start positions.
+	for(size_t i = 0; i < ten_triangles.size(); i++) {
+		EXPECT_EQ(i * 4, start_positions[0][i]); //Each triangle stores 4 vertices (of which one pivot).
+	}
 }
 
 }
